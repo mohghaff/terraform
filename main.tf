3800396d2a709a6a23bd150696c5d5ddbdbf729c
Mohsen <98326042+mohghaff@users.noreply.github.com>
2023-09-24 13:26:52 -0400

    blog id updated

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ module "alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = aws_instance.id
+          target_id = aws_instance.blog.id
           port = 80
         }
       }
